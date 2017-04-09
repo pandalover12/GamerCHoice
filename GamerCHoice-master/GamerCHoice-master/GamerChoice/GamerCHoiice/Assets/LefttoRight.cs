150a011dfe606accc58f2e64eb471f6f7f26c3bc
Arthur Parker <aeparker101@mail.com>
2017-04-09 16:55:44 -0400

    MEOW

---
+++ GamerCHoice-master/GamerCHoice-master/GamerChoice/GamerCHoiice/Assets/LefttoRight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LefttoRight : MonoBehaviour {
+    bool left;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x >= 5)
+            left = false;
+        if(transform.position.x<=-1.15f)
+        {
+            left = true;
+        }
+        if (left == false)
+        {
+            transform.Translate(-0.07f, 0, 0);
+        }
+        if(left==true)
+            transform.Translate(0.07f, 0, 0);
+    }
+}
