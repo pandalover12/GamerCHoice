cd6e88798ba49d8e9fdfe483131fa343c20db81d
Arthur Parker <aeparker101@mail.com>
2017-04-09 10:36:32 -0400

    Meow

---
+++ GamerChoice/GamerCHoiice/Assets/Scripts/ThemagicCarpet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThemagicCarpet : MonoBehaviour {
+    float speed = 4;
+    float rotating = 0;
+    public GameObject Destination;
+    public GameObject Player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(speed * Input.GetAxis("Left") * Time.deltaTime, 0f, speed * Input.GetAxis("Down") * Time.deltaTime);
+        transform.rotation   = Quaternion.Euler(transform.rotation.eulerAngles.y, rotating, rotating);
+        if(Player.transform.position.y<-4)
+        {
+            Player.transform.position = Destination.transform.position;
+        }
+	}
+}
