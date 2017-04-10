c7b0de37ec3ed3e5a1af33a7db747201ea928165
Arthur Parker <aeparker101@mail.com>
2017-04-09 20:38:45 -0400

    jh

---
+++ GamerChoice/GamerCHoiice/Assets/Rotatings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotatings : MonoBehaviour {
+    ThemagicCarpet cat = new ThemagicCarpet();
+    bool mewo;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mewo = cat.GetDragon();
+        
+        transform.Rotate(0.4f,0, 0);
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (mewo == true)
+        {
+            other.transform.parent = transform;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(mewo==false)
+        other.transform.parent = null;
+    }
+}
---
+++ GamerChoice/GamerCHoiice/GamerChoice/GamerCHoiice/GamerCHoiice/Assets/FakeROllingball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeROllingball : MonoBehaviour
+{
+    public Transform Player;
+    public Transform meow;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.transform.position.z < -4)
+        {
+            transform.LookAt(Player);
+            transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+        }
+    }
+    void  OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            this.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+}
+
