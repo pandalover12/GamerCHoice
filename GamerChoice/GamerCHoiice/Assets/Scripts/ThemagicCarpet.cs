0debdbda8bca4f5da50c748642643accd09db510
Arthur Parker <aeparker101@mail.com>
2017-04-10 09:13:11 -0400

    Meow1212

--- GamerChoice/GamerCHoiice/Assets/Scripts/ThemagicCarpet.cs
+++ GamerChoice/GamerCHoiice/Assets/Scripts/ThemagicCarpet.cs
@@ -5,16 +5,16 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class ThemagicCarpet : MonoBehaviour {
-    float speed = 4;
+   public float speed = 4;
     float rotating = 0;
     public int Heath = 5;
-    public int Sticky = 0;
-   public bool dragon = false;
-   public float counter = 0;
+    public int Speedy = 5;
+    bool dragon = false;
     public GameObject Destination;
     public GameObject Player;
     public string LoadbyLevl;
     public Text HeathText;
+    public Text SpeedTimer;
     
 	// Use this for initialization
 	void Start () {
@@ -23,21 +23,20 @@ public class ThemagicCarpet : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Space))
-             {
-            do
-            {
-                dragon = true;
-                counter += Time.deltaTime;
-            } while (counter <= 5);
-            dragon = false;
-            Sticky--;
-        }
-
-
         HeathText.text = "Heath" + Heath;
         transform.Translate(speed * Input.GetAxis("Left") * Time.deltaTime, 0f, speed * Input.GetAxis("Down") * Time.deltaTime);
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.y, rotating, rotating);
+        if (Input.GetKeyDown("space"))
+        {
+           speed =speed *2;
+            transform.Translate(speed * Input.GetAxis("Left") * Time.deltaTime, 0f, speed * Input.GetAxis("Down") * Time.deltaTime);
+        }
+
+
+        speed = 4;
+
+
+        
         if (Player.transform.position.y < -4 && Heath != 0)
         {
 
@@ -64,9 +63,9 @@ public class ThemagicCarpet : MonoBehaviour {
         {
             Heath--;
         }
-        if (other.gameObject.tag == "Sticky" )
+        if (other.gameObject.tag == "SpeedBoost" )
         {
-            Sticky++;
+            Speedy++;
           
         } 
       
